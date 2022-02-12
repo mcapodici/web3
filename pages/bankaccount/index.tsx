127282833aa66ac7b7dd6d31419335bf88277c89
Martin Capodici <martin.capodici@gmail.com>
2022-02-12 12:15:18 +1100

    bank account app mockup

---
+++ pages/bankaccount/index.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from "next";
+import Layout from "components/Layout";
+import { Form, Input, Button, Table } from "semantic-ui-react";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Bank Account</h1>
+      <p>
+        The bank account example is a simple smart contract to which you can
+        deposit and withdraw Ether. A contract is deployed per bank account, and
+        that bank account will only allow withdrawals to the creator of the
+        contract. Anyone can deposit Ether.
+      </p>
+      <h2>Your bank accounts</h2>
+      <p>
+        Here is a list of bank accounts connected to your current metamask
+        address "XYZ"
+      </p>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.Cell>Contract Address</Table.Cell>
+            <Table.Cell>Balance (Ether)</Table.Cell>
+            <Table.Cell>Actions</Table.Cell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>0x0883</Table.Cell>
+            <Table.Cell>0.01</Table.Cell>
+            <Table.Cell>
+              <Button primary>
+                Deposit
+              </Button>
+              <Button primary>
+                Withdraw
+              </Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <h2>Create bank accounts</h2>
+      <p>
+        You can create a bank account here. The initial deposit can be zero, if
+        you like.
+      </p>
+      <Form>
+        <Form.Field>
+          <label>Initial Deposit (Ether)</label>
+          <Input type="number" />
+        </Form.Field>
+        <Button primary>Create Account</Button>
+      </Form>
+    </Layout>
+  );
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/Layout'
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <Layout>My page
+    <Layout>
+      <h1>Welcome</h1>
+      <span>Welcome to <strong>web3</strong> home, click the examples on the left to try out web 3 examples. Or, try the </span>
+      <Link href='/bankaccount'>bank acccout</Link> example to get started.
     </Layout>
   )
 }
