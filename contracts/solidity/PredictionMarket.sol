d5bf200f62a823c0ea77e45a0f6c67d6b2ab446a
Martin Capodici <martin.capodici@gmail.com>
2022-02-21 20:43:23 +1100

    changed PredictionMarket.sol to use a v0 CID because it is simpler for now, and added some testing that we can indeed get the CID back.

--- contracts/solidity/PredictionMarket.sol
+++ contracts/solidity/PredictionMarket.sol
@@ -8,30 +8,29 @@ contract PredictionMarket {
     uint constant tokenToBalance = 1000000000000000000; // 18 DP.
     uint constant freeTokensOnRegistration = 10;
     uint constant freeBalanceOnRegistration = tokenToBalance * freeTokensOnRegistration;
-    
-    struct Multihash {
-      bytes32 hash;
-      uint8 hash_function;
-      uint8 size;
-    }
-
+  
     struct User {
       bytes32 username; // Username encoded in ascii maybe?
       uint balance; // Balances are implied 18 decimals
-      Multihash userinfo; // Link to find this on IPFS      
+      bytes32 userinfoMultihash; // Link to find this on IPFS      
     }
 
     mapping(address => User) public users;
     mapping(bytes32 => address) public usernames;
 
-    function register(bytes32 username, Multihash calldata userinfo) public {
+    /// @notice User self-registration
+    /// @param username ASCII encoded username
+    /// @param userinfoMultihash The multihash for user profile content, using a v0 CID.
+    /// @dev Further information on CIDS: https://docs.ipfs.io/concepts/content-addressing/#identifier-formats
+    /// @dev Application-defined interpretation of how to make use of the data stored at the CID address.
+    function register(bytes32 username, bytes32 userinfoMultihash) public {
         require(username != 0);
         require(usernames[username] == address(0));
         require(users[msg.sender].username == 0);
 
         User memory user;
         user.username = username;
-        user.userinfo = userinfo;
+        user.userinfoMultihash = userinfoMultihash;
         user.balance = freeBalanceOnRegistration;
         users[msg.sender] = user;
         usernames[username] = msg.sender;
--- contracts/test/PredictionMarket.test.ts
+++ contracts/test/PredictionMarket.test.ts
@@ -4,6 +4,8 @@ import { deployContract, MockProvider, solidity } from 'ethereum-waffle';
 import chaiSubset from 'chai-subset';
 import PredictionMarket from '../build/PredictionMarket.json';
 import BN from 'bn.js';
+import { CID, MultihashDigest } from 'multiformats/cid';
+import * as  Digest from 'multiformats/hashes/digest'
 
 use(chaiSubset);
 use(solidity);
@@ -12,9 +14,30 @@ config.truncateThreshold = 0;
 
 const username1 = '0x0000000000000000000000000000000000000000000000000000000074657374';
 const username2 = '0x0000000000000000000000000000000000000000000000000000000074657375';
-const userInfo1 = { hash: '0x0000000000000000000000000000000000000000000000000000000000000000', hash_function: 0, size: 0 };
+
+// Not strictly necessary fo the tests to use CID, but like to keep it here as a 'proof' that the contract
+// can interact with a CID
+const getMultihashForContract = (cid: CID) => '0x' + Buffer.from(cid.multihash.digest).toString('hex');
+const userinfoCID = CID.parse('QmSYuMAoJTNwDvH6pHCgHEgR5RvPr6fV7h6QDVHbdSGrGq');
+const userinfoMultihash1 = getMultihashForContract(userinfoCID);
+
+console.log(userinfoMultihash1);
 
 describe('PredictionMarket contract', () => {
+
+  describe('cid encoding', () => {
+    it('works - proof of concept', async () => {
+      // Prove that we can go back from userinfoMultihash1 to the CID
+      const mutlihashBytes = Uint8Array.from(Buffer.from(userinfoMultihash1.substr(2), 'hex'));
+      const headerBytes = Uint8Array.from([0x12, 0x20]);
+      const bytes = new Uint8Array(34);
+      bytes.set(headerBytes);
+      bytes.set(mutlihashBytes, 2);
+      const userinfoMultihash1FromStoredData = CID.decode(bytes);
+      expect(userinfoMultihash1FromStoredData.toString()).to.equal(userinfoCID.toString());
+    })
+  })
+
   describe('user registration', () => {
     const [wallet, wallet2] = new MockProvider().getWallets();
     let predictionMarket: Contract;
@@ -24,42 +47,42 @@ describe('PredictionMarket contract', () => {
     });
 
     it('allows registration', async () => {
-      await predictionMarket.register(username1, userInfo1);
+      await predictionMarket.register(username1, userinfoMultihash1);
 
       const userFromCall = await predictionMarket.users(wallet.address);
       expect(userFromCall.username, 'username').to.equal(username1);
       expect(userFromCall.balance.toString()).to.equal('10000000000000000000');
-      expect(userFromCall.userinfo).to.containSubset(userInfo1);
+      expect(userFromCall.userinfoMultihash).to.containSubset(userinfoMultihash1);
 
       const addressFromCall = await predictionMarket.usernames(username1);
       expect(addressFromCall).to.equal(wallet.address);
     });
 
     it('forbids registering the same address again', async () => {
-      await predictionMarket.register(username1, userInfo1);
-      await expect(predictionMarket.register(username1, userInfo1)).to.be.reverted;
+      await predictionMarket.register(username1, userinfoMultihash1);
+      await expect(predictionMarket.register(username1, userinfoMultihash1)).to.be.reverted;
     });
 
     it('forbids registering the same username again', async () => {
-      await predictionMarket.register(username1, userInfo1);
+      await predictionMarket.register(username1, userinfoMultihash1);
       const predictionMarketW2 = predictionMarket.connect(wallet2);
-      await expect(predictionMarketW2.register(username1, userInfo1)).to.be.reverted;
+      await expect(predictionMarketW2.register(username1, userinfoMultihash1)).to.be.reverted;
     });
 
     it('allows registration of another user if just username is different', async () => {
-      await predictionMarket.register(username1, userInfo1);
+      await predictionMarket.register(username1, userinfoMultihash1);
       const predictionMarketW2 = predictionMarket.connect(wallet2);
-      await expect(predictionMarketW2.register(username2, userInfo1)).to.not.be.reverted;
+      await expect(predictionMarketW2.register(username2, userinfoMultihash1)).to.not.be.reverted;
     });
 
     it('forbids registering with zero username', async () => {
-      await expect(predictionMarket.register('0x0000000000000000000000000000000000000000000000000000000000000000', userInfo1)).to.be.reverted;
+      await expect(predictionMarket.register('0x0000000000000000000000000000000000000000000000000000000000000000', userinfoMultihash1)).to.be.reverted;
     });
 
     it('emits registration events', async () => {
-      await predictionMarket.register(username1, userInfo1);
+      await predictionMarket.register(username1, userinfoMultihash1);
       const predictionMarketW2 = predictionMarket.connect(wallet2);
-      await predictionMarketW2.register(username2, userInfo1);
+      await predictionMarketW2.register(username2, userinfoMultihash1);
       const events = await predictionMarketW2.queryFilter({});
       expect(events).to.be.length(2);
       expect(events[0].args).to.containSubset({ username: username1, originaladdress: wallet.address });
