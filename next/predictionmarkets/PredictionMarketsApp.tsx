1cd3082f08e05d74cdf18cbb0546c80c635d6a9e
Your Name Here <your@email.com>
2022-03-01 09:43:10 +1100

    link to contract

--- next/predictionmarkets/PredictionMarketsApp.tsx
+++ next/predictionmarkets/PredictionMarketsApp.tsx
@@ -3,21 +3,16 @@ import Layout from "sitewide/Layout";
 import ShortAddressWithLink from "sitewide/ShortAddressWithLink";
 import { Web3Props } from "sitewide/RequireWeb3Wrapper";
 import Description from "./Description";
-
-interface IBankAccountDetails {
-  contractAddress: string;
-  balanceEther: string;
-}
-
-interface IShowDespoitWithdrawalModalInfo {
-  contractAddress: string;
-  isDeposit: boolean;
-}
+import siteWideData from "sitewide/SiteWideData.json";
 
 const PredictionMarketsApp = ({ web3Ref, firstAccount }: Web3Props) => {
   return (
     <Layout>
       <Description />
+      <p>
+        Note that this is using the contract deployed to{" "}
+        <ShortAddressWithLink address={siteWideData.deployedContractAddresses.predictionMarketsAddress} />
+      </p>
     </Layout>
   );
 };
