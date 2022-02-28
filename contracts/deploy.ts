92d591192eb95bb4abf01c00bee74525c6d1f036
Your Name Here <your@email.com>
2022-03-01 09:38:04 +1100

    code to allow different contracts to be deployed

--- contracts/deploy.ts
+++ contracts/deploy.ts
@@ -1,10 +1,17 @@
 import Web3 from 'web3';
 import HDWalletProvider from '@truffle/hdwallet-provider';
-import bankAccountFactoryMetaData from './build/BankAccountFactory.json';
 import { AbiItem } from 'web3-utils';
 import dotEnv from 'dotenv';
 
 dotEnv.config();
+const args = process.argv.slice(2);
+
+if (!args[0]) {
+    console.log('Usage: deploy CONTRACTNAME');
+    process.exit(1);
+}
+
+const metaData = require(`./build/${args[0]}.json`)
 const mnemonic_phrase = process.env.MNEMONIC_PHRASE;
 const provider_url = process.env.PROVIDER_URL;
 
@@ -19,9 +26,9 @@ const provider_url = process.env.PROVIDER_URL;
 
     const web3 = new Web3(provider);
     const addresses = await web3.eth.getAccounts();
-    const contract = new web3.eth.Contract(bankAccountFactoryMetaData.abi as AbiItem[]);
+    const contract = new web3.eth.Contract(metaData.abi as AbiItem[]);
 
-    const deployedContract = await contract.deploy({data: bankAccountFactoryMetaData.evm.bytecode.object}).send({from: addresses[0], gas: 2000000});
+    const deployedContract = await contract.deploy({data: metaData.evm.bytecode.object}).send({from: addresses[0], gas: 3000000});
     console.log('deployed to ' + deployedContract.options.address);
 
 })().catch(console.error);
