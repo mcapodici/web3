d9d6d319f72dbf388500137a41b3c37ff9554323
Your Name Here <your@email.com>
2022-03-04 09:21:00 +1100

    fixed description when no eth provider for predictionmarkets

--- next/pages/predictionmarkets/index.tsx
+++ next/pages/predictionmarkets/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import App from "predictionmarkets/PredictionMarketsApp";
 import { RequireWeb3Wrapper } from "sitewide/RequireWeb3Wrapper";
-import Description from "bankaccount/Description";
+import Description from "predictionmarkets/Description";
 
 const Home: NextPage = RequireWeb3Wrapper(App, Description);
 export default Home;
