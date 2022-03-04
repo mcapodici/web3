3c5fd6d2b638dc475a7f3b8cb4b9be3b3331384b
Your Name Here <your@email.com>
2022-03-05 09:41:13 +1100

    routing from markets to market

---
+++ next/pages/predictionmarkets/markets/[marketid]/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import App from "predictionmarkets/PredictionMarketsApp";
+import { RequireWeb3Wrapper } from "sitewide/RequireWeb3Wrapper";
+import Description from "predictionmarkets/Description";
+
+const Home: NextPage = () => {
+    return <div>
+        Hello
+    </div>
+};
+export default Home;
--- next/predictionmarkets/Markets.tsx
+++ next/predictionmarkets/Markets.tsx
@@ -2,6 +2,8 @@ import { Card, Icon, Image } from "semantic-ui-react";
 import siteWideData from "sitewide/SiteWideData.json";
 import { toNumberOfTokens } from "util/BN";
 import { formatDistance } from "date-fns";
+import Router from "next/router";
+import Link from "next/link";
 
 export interface IMarketsProps {
   markets: any[];
@@ -14,56 +16,76 @@ const Markets = ({ markets }: IMarketsProps) => {
         .slice()
         .sort((a, b) => b.blockNumber - a.blockNumber)
         .map((market) => (
-          <Card>
-            <Card.Content>
-              <Card.Header>
-                <Image
-                  floated="left"
-                  size="small"
-                  src={siteWideData.imageHashTemplate.replace(
-                    "{0}",
-                    `${market.username}_${market.index}`
-                  )}
-                  ui={true}
-                  circular={true}
-                />
-                {(market.title || "Untitled").substring(0, 50)}
-              </Card.Header>
-              <div>
-                <Card.Meta>
-                  <span className="date">By {market.username}</span>
-                </Card.Meta>
-                <Card.Description>
-                  {(market.description || "").substring(0, 100)}...
-                </Card.Description>
-              </div>
-            </Card.Content>
-            <Card.Content extra>
-              <a title="Money bet on market, including market maker" style={{marginRight:'10px'}}>
-                <Icon name="money" color="yellow" />
-                {toNumberOfTokens(market.poolsize).toString()}
-              </a>
-              <a title="Number of bettors, including market maker">
-                <Icon name="user" color="brown" />
-                {market.bets.length + 1}
-              </a>
-            </Card.Content>
-            <Card.Content extra>
-              <a title={"Created at " + market.timestamp.toLocaleString() + " (your local time)" } style={{marginRight:'10px'}}>
-                <Icon name="clock" color="green" />
-                {formatDistance(new Date(market.timestamp), new Date(), {
-                  addSuffix: true,
-                })}
-              </a>
-              &nbsp; &nbsp;
-              <a title={"Closes at " + market.closesAt.toLocaleString() + " (your local time)"}>
-                <Icon name="clock" color="black" />
-                {formatDistance(new Date(market.closesAt), new Date(), {
-                  addSuffix: true,
-                })}
-              </a>
-            </Card.Content>
-          </Card>
+          <Link
+            href={`/predictionmarkets/markets/${market.useraddress}_${market.index}`}
+          >
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                  <Image
+                    floated="left"
+                    size="small"
+                    src={siteWideData.imageHashTemplate.replace(
+                      "{0}",
+                      `${market.username}_${market.index}`
+                    )}
+                    ui={true}
+                    circular={true}
+                  />
+                  {(market.title || "Untitled").substring(0, 50)}
+                </Card.Header>
+                <div>
+                  <Card.Meta>
+                    <span className="date">By {market.username}</span>
+                  </Card.Meta>
+                  <Card.Description>
+                    {(market.description || "").substring(0, 100)}...
+                  </Card.Description>
+                </div>
+              </Card.Content>
+              <Card.Content extra>
+                <a
+                  title="Money bet on market, including market maker"
+                  style={{ marginRight: "10px" }}
+                >
+                  <Icon name="money" color="yellow" />
+                  {toNumberOfTokens(market.poolsize).toString()}
+                </a>
+                <a title="Number of bettors, including market maker">
+                  <Icon name="user" color="brown" />
+                  {market.bets.length + 1}
+                </a>
+              </Card.Content>
+              <Card.Content extra>
+                <a
+                  title={
+                    "Created at " +
+                    market.timestamp.toLocaleString() +
+                    " (your local time)"
+                  }
+                  style={{ marginRight: "10px" }}
+                >
+                  <Icon name="clock" color="green" />
+                  {formatDistance(new Date(market.timestamp), new Date(), {
+                    addSuffix: true,
+                  })}
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  title={
+                    "Closes at " +
+                    market.closesAt.toLocaleString() +
+                    " (your local time)"
+                  }
+                >
+                  <Icon name="clock" color="black" />
+                  {formatDistance(new Date(market.closesAt), new Date(), {
+                    addSuffix: true,
+                  })}
+                </a>
+              </Card.Content>
+            </Card>
+          </Link>
         ))}
     </Card.Group>
   );
