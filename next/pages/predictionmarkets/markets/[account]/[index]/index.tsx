37d404f6b1d0ade753c8cee7a9fae06914cf22c0
Your Name Here <your@email.com>
2022-03-05 20:28:56 +1100

    check if registered to bet

--- next/pages/predictionmarkets/markets/[account]/[index]/index.tsx
+++ next/pages/predictionmarkets/markets/[account]/[index]/index.tsx
@@ -2,13 +2,17 @@ import type { NextPage } from "next";
 import { RequireWeb3Wrapper, Web3Props } from "sitewide/RequireWeb3Wrapper";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { getMarket, getUserInfo, UserInfo } from "ethereum/contracts/PredictionMarket";
+import {
+  getMarket,
+  getUserInfo,
+  UserInfo,
+} from "ethereum/contracts/PredictionMarket";
 import Layout from "sitewide/Layout";
 import { formatDistance } from "date-fns";
 import { Button, Card, Form, Icon, Image, Input } from "semantic-ui-react";
 import { toNumberOfTokens } from "util/BN";
 import siteWideData from "sitewide/SiteWideData.json";
-import BN from 'bn.js';
+import BN from "bn.js";
 
 const Index: NextPage<Web3Props> = ({ web3Ref, firstAccount }: Web3Props) => {
   const [market, setMarket] = useState<any>();
@@ -16,21 +20,21 @@ const Index: NextPage<Web3Props> = ({ web3Ref, firstAccount }: Web3Props) => {
   const [userInfo, setUserInfo] = useState<UserInfo | undefined>();
   const r = useRouter();
   const betFormErrorMessage = false;
-  
-  const intPattern = /^[0-9]+$/
 
-  const web3 =  web3Ref.current;
+  const intPattern = /^[0-9]+$/;
+
+  const web3 = web3Ref.current;
   const betAmountBN = new BN(betAmount);
   const funds = userInfo?.balance || new BN("0");
-  const betAmountError = !intPattern.test(betAmount) || betAmountBN.lt(new BN(1)) || betAmountBN.gt(new BN(funds));
+  const isRegistered = !!userInfo?.username;
+  const betAmountError =
+    !intPattern.test(betAmount) ||
+    betAmountBN.lt(new BN(1)) ||
+    betAmountBN.gt(new BN(funds));
 
   const loadMarket = async () => {
     const { account, index } = r.query;
-    const m = await getMarket(
-      web3,
-      account as string,
-      index as string
-    );
+    const m = await getMarket(web3, account as string, index as string);
     setMarket(m);
   };
 
@@ -119,22 +123,31 @@ const Index: NextPage<Web3Props> = ({ web3Ref, firstAccount }: Web3Props) => {
       )}
 
       <h2>Place your bet</h2>
-      <Form error={!!betFormErrorMessage}>
-        <Form.Field error={betAmountError}>
-          <Input
-            label="P$"
-            labelPosition="left"
-            value={betAmount}
-            onChange={(event) => setBetAmount(event.target.value)}
-            action={
-              <>
-                <Button style={{marginLeft:'5px', marginRight:'5px'}} color="green">Bet YES</Button>
-                <Button color="pink">Bet NO</Button>
-              </>
-            }
-          ></Input>
-        </Form.Field>
-      </Form>
+      {isRegistered ? (
+        <Form error={!!betFormErrorMessage}>
+          <Form.Field error={betAmountError}>
+            <Input
+              label="P$"
+              labelPosition="left"
+              value={betAmount}
+              onChange={(event) => setBetAmount(event.target.value)}
+              action={
+                <>
+                  <Button
+                    style={{ marginLeft: "5px", marginRight: "5px" }}
+                    color="green"
+                  >
+                    Bet YES
+                  </Button>
+                  <Button color="pink">Bet NO</Button>
+                </>
+              }
+            ></Input>
+          </Form.Field>
+        </Form>
+      ) : (
+        <p>Register to bet</p>
+      )}
     </div>
   ) : (
     <p>Loading...</p>
