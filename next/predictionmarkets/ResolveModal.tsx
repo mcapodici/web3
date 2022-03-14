0d5c932335dd4909a132823457df5af7b2d703a5
Martin Capodici <martin.capodici@gmail.com>
2022-03-14 20:51:00 +1100

    use web3action flow in resolve

--- next/predictionmarkets/ResolveModal.tsx
+++ next/predictionmarkets/ResolveModal.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Modal } from "semantic-ui-react";
 import { Web3Props } from "sitewide/RequireWeb3Wrapper";
 import * as Contract from "ethereum/contracts/PredictionMarket";
-import { Alert, AlertPanel, AlertType } from "sitewide/alerts/AlertPanel";
+import useWeb3Action from "sitewide/hooks/useWeb3Action";
 
 export interface RegisterModalProps extends Web3Props {
   onClose: () => void;
@@ -15,43 +15,26 @@ const ResolveModal = ({
   marketIndex,
   onClose,
 }: RegisterModalProps) => {
-  const [error, setError] = useState("");
+  const web3Action = useWeb3Action();
 
   const resolve = async (isYes: boolean) => {
-    setError("");
-    try {
-      await Contract.resolve(
-        web3Ref.current,
-        firstAccount,
-        marketIndex,
-        isYes ? 1 : 0
-      );
-    } catch (ex: any) {
-      const userMessage = "Details from provider: " + ex.message;
-      setError(userMessage);
-      return; // Keep modal open so message can be seen.
-    }
     onClose();
+    await web3Action(
+      "Your market is being resolved. Please follow the steps shown by your Ethereum provider.",
+      "Resolving Market",
+      () =>
+        Contract.resolve(
+          web3Ref.current,
+          firstAccount,
+          marketIndex,
+          isYes ? 1 : 0
+        )
+    );
   };
-
-  const alerts: Alert[] =
-    error === ""
-      ? []
-      : [
-          {
-            content: error,
-            header: "Error occurred during registration",
-            type: AlertType.Negative,
-            uniqueId: "predictionmarket.registermodal.error",
-          },
-        ];
 
   return (
     <Modal size={"mini"} open={true} onClose={() => onClose()} closeIcon>
       <Modal.Header>Resolve This Market</Modal.Header>
-      <Modal.Content>
-        <AlertPanel alerts={alerts} onDismiss={() => setError("")} />
-      </Modal.Content>
       <Modal.Actions>
         <Button color="green" onClick={() => resolve(true)}>
           Resolve YES
